0e61b35373269c9c59bddd4b94c8747b4f0669e2
narayan9096 <narayanpawar9096@gmail.com>
2024-12-22 14:22:44 +0530

    Update EmployeeRepository.java
    Added method in repository

--- src/main/java/com/example/employee/repository/EmployeeRepository.java
+++ src/main/java/com/example/employee/repository/EmployeeRepository.java
@@ -4,4 +4,5 @@ import com.example.employee.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+      List<Employee> findByPosition(String position);
 }
